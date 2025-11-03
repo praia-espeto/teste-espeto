fbda637daa20a9d9b5b8ce4409e029fd6d7af324
praia-espeto <sorocabanosespetoteste@gmail.com>
2025-11-03 15:49:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,19 +2,14 @@ document.getElementById('formNotion').addEventListener('submit', async (e) => {
   e.preventDefault();
 
   const nome = document.getElementById('nome').value;
-  const telefone = document.getElementById('telefone').value;
-  const endereco = document.getElementById('endereco').value;
-  const qtd = document.getElementById('qtd').value;
-  const item = document.getElementById('item').value;
-  const total = document.getElementById('total').value;
-
+  const status = document.getElementById('status').value;
   const mensagem = document.getElementById('mensagem');
 
   try {
     const resposta = await fetch('/.netlify/functions/enviarParaNotion', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ nome, telefone, endereco, qtd,item, total }),
+      body: JSON.stringify({ nome, status }),
     });
 
     const data = await resposta.json();
